c6fba985481fd784a3b64f9cf57501040313096e
dsknd <daisukeendo@outlook.jp>
2025-03-02 06:02:15 +0900

    プロジェクトロールテーブルの外部キーを変更

--- database/migrations/2025_01_02_122201_create_project_role_assignments_table.php
+++ database/migrations/2025_01_02_122201_create_project_role_assignments_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             // カラム定義
             $table->unsignedBigInteger('project_role_id');    // プロジェクトロールID
             $table->unsignedBigInteger('project_id');         // プロジェクトID
-            $table->unsignedBigInteger('user_id');            // ユーザーID
+            $table->unsignedBigInteger('assigned_user_id');   // 割当たられたユーザーID
+            $table->unsignedBigInteger('assigner_user_id');   // 割当たられたユーザーID
             $table->timestamps();
 
             // 外部キー制約
@@ -24,13 +25,18 @@ return new class extends Migration
                 ->on('project_roles')
                 ->cascadeOnDelete();
 
-            $table->foreign(['project_id', 'user_id'])
+            $table->foreign(['project_id', 'assigned_user_id'])
                 ->references(['project_id', 'user_id'])
                 ->on('project_members')
                 ->cascadeOnDelete();
 
+            $table->foreign('assigner_user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
+
             // 主キー制約
-            $table->primary(['project_id', 'user_id', 'project_role_id']);
+            $table->primary(['project_id', 'assigned_user_id', 'project_role_id']);
         });
     }
 

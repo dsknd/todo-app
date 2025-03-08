90ad2eef08416bb031c55d0bea9466b5b12160f9
dsknd <daisukeendo@outlook.jp>
2025-03-08 19:53:40 +0900

    temp

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -20,10 +20,14 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'description' => fake()->text(),
-            'is_private' => false,
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph,
+            'is_private' => $this->faker->boolean,
             'project_status_id' => ProjectStatus::factory(),
+            'planned_start_date' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'planned_end_date' => $this->faker->dateTimeBetween('+1 month', '+6 months'),
+            'actual_start_date' => $this->faker->optional(0.7)->dateTimeBetween('-1 month', 'now'),
+            'actual_end_date' => $this->faker->optional(0.3)->dateTimeBetween('+1 month', '+6 months'),
             'user_id' => User::factory(),
         ];
     }

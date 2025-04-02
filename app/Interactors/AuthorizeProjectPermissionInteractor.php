2195d22672e42207bc4e9da7543704770a116151
dsknd <daisukeendo@outlook.jp>
2025-04-02 22:51:49 +0900

    rename djoewk

--- app/Interactors/AuthorizeProjectPermissionInteractor.php
+++ app/Interactors/AuthorizeProjectPermissionInteractor.php
@@ -6,7 +6,7 @@ use App\UseCases\AuthorizeProjectPermissionUseCase;
 use App\ValueObjects\UserId;
 use App\ValueObjects\ProjectId;
 use App\ValueObjects\PermissionId;
-use App\Repositories\Interfaces\ProjectMemberQueryRepository;
+use App\Repositories\Interfaces\ProjectMemberPermissionQueryRepository;
 use App\Repositories\Interfaces\PermissionRepository;
 class AuthorizeProjectPermissionInteractor implements AuthorizeProjectPermissionUseCase
 {
@@ -15,7 +15,7 @@ class AuthorizeProjectPermissionInteractor implements AuthorizeProjectPermission
      * @param PermissionRepository $permissionRepository
      */
     public function __construct(
-        private readonly ProjectMemberQueryRepository $projectMemberQueryRepository,
+        private readonly ProjectMemberPermissionQueryRepository $projectMemberPermissionQueryRepository,
         private readonly PermissionRepository $permissionRepository,
     ) {}
 
@@ -25,7 +25,7 @@ class AuthorizeProjectPermissionInteractor implements AuthorizeProjectPermission
     public function execute(UserId $userId, ProjectId $projectId, PermissionId $permissionId): bool
     {
         // ロールを取得
-        $permissions = $this->projectMemberQueryRepository->findMemberPermissions($projectId, $userId);
+        $permissions = $this->projectMemberPermissionQueryRepository->findMemberPermissions($projectId, $userId);
 
         // ロールが持つ権限が、指定された権限を含むかどうかを確認
         return $this->permissionRepository->imply($permissions->getPermissionIds(), $permissionId);

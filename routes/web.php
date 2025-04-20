9aaa8449c4ba1ad2491f0cd8749e5bc11544f69c
dsknd <daisukeendo@outlook.jp>
2025-04-20 17:37:25 +0900

    temp

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
+
+// APIドキュメント
+Route::get('/documentation-api', function () {
+    return view('scramble::docs', [
+        'spec' => file_get_contents(base_path('api.json')),
+        'config' => Scramble::getGeneratorConfig('default'),
+    ]);
+})
+->middleware(Scramble::getGeneratorConfig('default')->get('middleware', [RestrictedDocsAccess::class]));
+
 Route::get('/', function () {
     return view('welcome');
 });
